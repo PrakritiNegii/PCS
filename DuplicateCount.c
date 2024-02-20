ef7debf27ad9d7dedd333df956d4b79b891cab4e
PrakritiNegii <160118895+PrakritiNegii@users.noreply.github.com>
2024-02-20 19:42:52 +0530

    Update DuplicateCount.c

--- DuplicateCount.c
+++ DuplicateCount.c
@@ -13,16 +13,16 @@ int main()
    {
     c = 0;
     for(int j=i+1; j<n; j++)
-     {
-      if(A[i]==A[j]) 
-       {
-        c++;
-        for(int k=j; k<n; k++)
+      {
+       if(A[i]==A[j]) 
          {
-           A[k] = A[k+1];
+          c++;
+          for(int k=j; k<n; k++)
+            {
+             A[k] = A[k+1];
+            }
+          n--;
          }
-        n--;
-       }
      }
     printf("\nThe number %d is present %d times in the input array.\n",i,c);
    }
